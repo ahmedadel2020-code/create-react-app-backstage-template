a955f80db794566a0b6431a8f9b690e5fab3e0d9
Ahmed ADEL-ABDELRAHMAN <ahmed.adel-abdelrahman.ext@valeo.com>
2024-10-09 16:50:54 +0300

    fix tests

--- skeleton/src/App.test.tsx
+++ skeleton/src/App.test.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// Mock the process.env variables
+beforeAll(() => {
+  process.env.REACT_APP_API_URL = "https://api.test.com";
+  process.env.REACT_APP_ENVIRONMENT = "test";
+});
+
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
